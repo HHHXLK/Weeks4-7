ebd285f7cd4e732326b60d58d17de13853c85fd9
HHHXLK <2639580737hxlk@gmail.com>
2026-02-20 17:13:00 -0500

    123

---
+++ Assets/Script/DamageHazard.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class DamageHazard : MonoBehaviour
+{
+    public int damage;
+    public DungeonRaiderPlayer player;
+
+    private SpriteRenderer hazardRenderer;
+    private bool wasInTheHazard = false;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        hazardRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isInTheHazard = hazardRenderer.bounds.Contains(player.transform.position);
+        if (isInTheHazard
+            && wasInTheHazard == false)
+        {
+            wasInTheHazard = true;
+            player.TakeDamage(damage);
+        }
+        //IF WE HAVE EXITED OUT OF THE HAZARD AND WE WERE PREVIOUSLY IN THE HAZARD
+        //THEN WE RESET
+        else if (!isInTheHazard && wasInTheHazard)
+        {
+            wasInTheHazard = false;
+        }
+
+
+    }
+}
