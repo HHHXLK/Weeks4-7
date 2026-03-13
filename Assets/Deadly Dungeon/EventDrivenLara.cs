5f8d2040eca6a653061c2d36b50e1529e7677dcb
HHHXLK <2639580737hxlk@gmail.com>
2026-03-13 06:35:55 -0400

    Lava lake

--- Assets/Deadly Dungeon/EventDrivenLara.cs
+++ Assets/Deadly Dungeon/EventDrivenLara.cs
@@ -24,7 +24,8 @@ public class EventDrivenLara : MonoBehaviour
     public UnityEvent<float> SetupSwimTimer; //pass max timer value. Use to set value to 0, max value, hide slider
     public UnityEvent StartSwimTimer;
     public UnityEvent StopSwimTimer;
-    
+
+    public float drowningDamagePerSecond = 1f;
 
     void Start()
     {
@@ -51,6 +52,11 @@ public class EventDrivenLara : MonoBehaviour
         SetAnimation();
 
         transform.position += (Vector3)movement;
+
+        if (isSwimming)
+        {
+            TakeDamage(drowningDamagePerSecond * Time.deltaTime);
+        }
     }
 
     public void SetSwimming(bool value)
